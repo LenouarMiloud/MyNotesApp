46ea4cc97414776a059f022b6eef7b738d7e2485
LenouarMiloud <lenouarmiloud@gmail.com>
2021-11-10 17:46:39 +0100

    modify some code

--- app/src/main/java/com/fsociety/mynotesapp/MainActivity.kt
+++ app/src/main/java/com/fsociety/mynotesapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.fsociety.mynotesapp
 
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cursoradapter.widget.SimpleCursorAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    var accessDB :SQLiteDatabase = null
+    var cursor : Cursor? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,16 +17,26 @@ class MainActivity : AppCompatActivity() {
         //create our Databse "myNotesDB"
         var objectDataBase = myNoteSQLiteOpenHelper(this)
         //access to our database
-        var accessDB = objectDataBase.readableDatabase
+        accessDB = objectDataBase.readableDatabase
         //get the data from our DataBase
-        var cursor = accessDB.query("myNotes", arrayOf("id","title"),
+        cursor = accessDB!!.query("myNotes", arrayOf("id","title"),
                 null,null,null,null,null,
                 )
 
         //creating the adapter
-        val listAdapter = SimpleCursorAdapter(this,android.R.layout.simple_list_item_1,cursor, arrayOf("title"), intArrayOf(android.R.id.text1))
-        
-
-
+        val listAdapter = SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1,
+                cursor,
+                arrayOf("title"),
+                intArrayOf(android.R.id.text1),
+                0
+        )
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cursor!!.close()
+        accessDB!!.close()
+    }
+
 }
