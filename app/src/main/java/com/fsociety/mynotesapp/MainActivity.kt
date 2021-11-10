8fa9d8fc25080c3033c34d9e54e53ad7e6aeb6ae
LenouarMiloud <lenouarmiloud@gmail.com>
2021-11-10 10:22:58 +0100

    creating the data source

--- app/src/main/java/com/fsociety/mynotesapp/MainActivity.kt
+++ app/src/main/java/com/fsociety/mynotesapp/MainActivity.kt
@@ -7,5 +7,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //create our Databse "myNotesDB"
+        var objectDataBase = myNoteSQLiteOpenHelper(this)
+        //access to our database
+        var accessDB = objectDataBase.readableDatabase
+        //get the data from our DataBase
+        var cursor = accessDB.query("myNotes", arrayOf("id","title"),
+                null,null,null,null,null,
+                )
+
+
+
+
     }
 }
