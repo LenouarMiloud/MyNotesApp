41ed3646960cd8303253c4e326cfeea921e94c44
LenouarMiloud <lenouarmiloud@gmail.com>
2021-11-10 10:16:45 +0100

    start the myNotesSQLite to manage our database

---
+++ app/src/main/java/com/fsociety/mynotesapp/myNoteSQLiteOpenHelper.kt
@@ -0,0 +1,21 @@
+package com.fsociety.mynotesapp
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class myNoteSQLiteOpenHelper(context: Context) : SQLiteOpenHelper(context,"myNotesDB",null,1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("CREATE TABLE myNotes)"+
+                "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +"title TEXT"
+                +"description TEXT)"
+        )
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
+
+}
