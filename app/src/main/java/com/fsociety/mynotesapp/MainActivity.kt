b045fa1329215864c9a421918ca0e368a58474b3
LenouarMiloud <lenouarmiloud@gmail.com>
2021-11-10 13:21:56 +0100

    customize our adapter

--- app/src/main/java/com/fsociety/mynotesapp/MainActivity.kt
+++ app/src/main/java/com/fsociety/mynotesapp/MainActivity.kt
@@ -2,12 +2,13 @@ package com.fsociety.mynotesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cursoradapter.widget.SimpleCursorAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        /*Creating the datasource*/
         //create our Databse "myNotesDB"
         var objectDataBase = myNoteSQLiteOpenHelper(this)
         //access to our database
@@ -17,7 +18,9 @@ class MainActivity : AppCompatActivity() {
                 null,null,null,null,null,
                 )
 
-
+        //creating the adapter
+        val listAdapter = SimpleCursorAdapter(this,android.R.layout.simple_list_item_1,cursor, arrayOf("title"), intArrayOf(android.R.id.text1))
+        
 
 
     }
